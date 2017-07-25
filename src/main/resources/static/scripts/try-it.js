807854b8ecca95132bb3ab76b12c8510fa8f81f4
quasarjohn <gilbertjohncuaresma@gmail.com>
2017-07-25 21:50:06 +0800

    preview classified image uploaded by user

--- src/main/resources/static/scripts/try-it.js
+++ src/main/resources/static/scripts/try-it.js
@@ -31,49 +31,7 @@ function progressbar() {
     return bar;
 }
 
-function classifyImageFromURL(img_url) {
-    //send async request to the server
-    var xhr = new XMLHttpRequest();
-    /*
-     as of the moment, the request is sent to localhost, but it should be sent later to the
-     dedicated server for image classification
-     */
-    xhr.open('GET', "http://192.168.0.137:8091/api/user1/classifier/classify_image/xxx?img_url=" +
-        encodeURIComponent(img_url) + "&max_results=5", true);
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send();
 
-    xhr.onreadystatechange = function () {
-        onClassificationDone();
-        if (this.readyState == 4 && this.status == 200) {
-            var progressList = document.getElementById('progressList');
-            var jsonData = JSON.parse(xhr.responseText);
-
-            for (var i = 0; i < jsonData.content.length; i++) {
-                var label = jsonData.content[i].label;
-                var probability = jsonData.content[i].probability * 100;
-                //probability to display
-                var probability_decimal = Number(Math.round(probability + 'e2') + 'e-2').toFixed(2);
-                //used for determining the width of the progressbar
-                var probability_int = Math.round(probability);
-
-                //the probabilities of the classifications are displayed using this piece of code.
-                // medyo baboy yung pagkakacode. haha
-                //TODO please remove the text from the background of the progressbar
-                $('#progressList').append("<div class='progress'><div class='progress-bar progress-bar-custom text' " +
-                    "role='progressbar' aria-valuenow='" + probability_int + "' aria-valuemin='0' aria-valuemax='100' " +
-                    "style='width: " + probability_int + "%;'><span class='text-center'>" + probability_decimal + "% " +
-                    "Probability</span></div></div>")
-
-                $('#labelsList').append("<p>" + label + "&ensp;</p>")
-            }
-
-            document.getElementById('test_content').innerHTML = xhr.responseText;
-            $('#resultImg').attr('src', img_url);
-        }
-    };
-}
 
 //on page load, hide some elements that will only be shown when classifying images
 $(document).ready(function () {
@@ -101,7 +59,6 @@ $(document).ready(function () {
 
 //block for classifying images using url
 $(document).ready(function () {
-    onClassificationStarted();
     $("#classifyURLBtn").click(function () {
         onClassificationStarted();
         var img_url = document.getElementById('img_url_input').value;
@@ -180,7 +137,7 @@ function ajax_classify_image() {
         },
         //user1 is the temporary api key
         enctype: 'multipart/form-data',
-        url: "http://192.168.0.137:8091/api/user1/classifier/upload_classify_image/modelkey",
+        url: "http://192.168.0.149:8091/api/user1/classifier/upload_classify_image/modelkey",
         data: data,
         processData: false, //prevent jQuery from automatically transforming the data into a query string
         contentType: false,
@@ -199,6 +156,50 @@ function ajax_classify_image() {
             console.log("ERROR : ", e);
         }
     });
+}
+
+function classifyImageFromURL(img_url) {
+    //send async request to the server
+    var xhr = new XMLHttpRequest();
+    /*
+     as of the moment, the request is sent to localhost, but it should be sent later to the
+     dedicated server for image classification
+     */
+    xhr.open('GET', "http://192.168.0.149:8091/api/user1/classifier/classify_image/xxx?img_url=" +
+        encodeURIComponent(img_url) + "&max_results=5", true);
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        onClassificationDone();
+        if (this.readyState == 4 && this.status == 200) {
+            var progressList = document.getElementById('progressList');
+            var jsonData = JSON.parse(xhr.responseText);
+
+            for (var i = 0; i < jsonData.content.length; i++) {
+                var label = jsonData.content[i].label;
+                var probability = jsonData.content[i].probability * 100;
+                //probability to display
+                var probability_decimal = Number(Math.round(probability + 'e2') + 'e-2').toFixed(2);
+                //used for determining the width of the progressbar
+                var probability_int = Math.round(probability);
+
+                //the probabilities of the classifications are displayed using this piece of code.
+                // medyo baboy yung pagkakacode. haha
+                //TODO please remove the text from the background of the progressbar
+                $('#progressList').append("<div class='progress'><div class='progress-bar progress-bar-custom text' " +
+                    "role='progressbar' aria-valuenow='" + probability_int + "' aria-valuemin='0' aria-valuemax='100' " +
+                    "style='width: " + probability_int + "%;'><span class='text-center'>" + probability_decimal + "% " +
+                    "Probability</span></div></div>")
+
+                $('#labelsList').append("<p>" + label + "&ensp;</p>")
+            }
+
+            document.getElementById('test_content').innerHTML = xhr.responseText;
+            $('#resultImg').attr('src', img_url);
+        }
+    };
 }
 
 function displayClassificationResults(data) {
