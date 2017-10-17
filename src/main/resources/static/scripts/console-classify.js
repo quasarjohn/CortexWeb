dd669db26dec5c95f81d705043788aa862357773
quasarjohn <quasarjohn21@gmail.com>
2017-10-17 23:12:41 +0800

    function for console classify. not yet complete

--- src/main/resources/static/scripts/console-classify.js
+++ src/main/resources/static/scripts/console-classify.js
@@ -1,3 +1,6 @@
+var model_key = null;
+var api_key = null;
+
 //On page load
 $(document).ready(function () {
     $("#outputImage").hide();
@@ -7,28 +10,15 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-function output() {
-    $("#progressBar").hide();
-    $("#progressBarMessage").hide();
-    $("#outputImage").show();
-    $(".outputPanels").show();
-}
-
-$("#classifyURLBtn").click(function () {
-    var hideOnLoad = document.getElementById("hideOnLoad");
-    hideOnLoad.style.display = hideOnLoad.style.display.replace("none", "block");
-
-    $("#outputImage").hide();
-    $(".outputPanels").hide();
-    $("#progressBar").show();
-    $("#progressBarMessage").show();
-    setTimeout(output, 3000);
-});
-
 
 $(document).ready(function () {
-    loadClassifiers();
-    listenToClassifierSelection();
+    var xhr = requestJson(getWebServerAddress() + "/session/user-info");
+    xhr.onreadystatechange = function () {
+        var user = JSON.parse(xhr.responseText);
+        api_key = user.api_key;
+
+        loadClassifiers();
+    }
 });
 
 function loadClassifiers() {
@@ -46,7 +36,7 @@ function loadClassifiers() {
         console.log(classifiers_request);
         xhr2.onreadystatechange = function () {
             var classifiers = JSON.parse(xhr2.responseText);
-            // $(".dropdown-menu").empty();
+            $(".dropdown-menu").empty();
             populateClassifiersList(classifiers.content);
         }
     }
@@ -54,17 +44,117 @@ function loadClassifiers() {
 
 function populateClassifiersList(classifiers) {
     classifiers.forEach(function (classifier) {
-        console.log(classifier.title);
-        $(".dropdown-menu").append("<li><a href=\"#\">" + classifier.title + "</a></li>")
+        $(".dropdown-menu").append("<li id='" + classifier.model_key + "'><a href=\"#\">" + classifier.title + "</a></li>")
     });
+    listenToClassifierSelection();
 }
 
 function listenToClassifierSelection() {
-
     $(".dropdown-menu li").click(function () {
         alert("CLASSIFIER SELECTED");
-        console.log("CLASSIFIER SELECTED");
-        alert(this);
-    })
+        // console.log("CLASSIFIER SELECTED");
+        alert(this.id);
+        model_key = this.id.toString();
+        alert(model_key);
+    });
+}
 
+//uploads file using ajax
+function ajax_classify_image() {
+    alert(model_key);
+    onClassificationStarted();
+    var form = $('#fileUploadForm')[0];
+    var data = new FormData(form);
+
+    data.append("key", "testVALUE");
+
+    $.ajax({
+        type: "POST",
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+            'Access-Control-Allow-Credentials': true,
+        },
+        //user1 is the temporary api key
+        enctype: 'multipart/form-data',
+        url: getServerAddress() + "/api/" + api_key + "/classifier/upload_classify_image/" + model_key,
+        data: data,
+        processData: false, //prevent jQuery from automatically transforming the data into a query string
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            // displayClassificationResults(data);
+            var str_data = JSON.stringify(data);
+            onClassificationDone();
+            console.log(str_data);
+            //document.getElementById('test_content').innerHTML = str_data;
+            displayClassificationResults(str_data);
+        },
+        error: function (e) {
+            onClassificationDone();
+            console.log("ERROR : ", e);
+        }
+    });
+}
+
+function onClassificationStarted() {
+
+    var hideOnLoad = document.getElementById("hideOnLoad");
+    hideOnLoad.style.display = hideOnLoad.style.display.replace("none", "block");
+
+    $("#outputImage").hide();
+    $(".outputPanels").hide();
+    $("#progressBar").show();
+    $("#progressBarMessage").show();
+}
+
+function onClassificationDone() {
+    $("#outputImage").show();
+    $(".outputPanels").show();
+    $("#progressBar").hide();
+    $("#progressBarMessage").hide();
+}
+
+// block for classifying image uploaded by user
+$(document).ready(function () {
+    $("#fileChooserBtn").click(function () {
+        $("#file1").click();
+    });
+
+    var input = document.getElementById('file1');
+    $("#file1").change(function (event) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#resultImg').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        $("#btnSubmit").click();
+    });
+
+    $("#btnSubmit").click(function (event) {
+        event.preventDefault();
+        ajax_classify_image();
+    });
+
+});
+
+function displayClassificationResults(data) {
+    var jsonData = JSON.parse(data);
+    for (var i = 0; i < jsonData.content.length; i++) {
+        var label = jsonData.content[i].label;
+        var probability = jsonData.content[i].probability * 100;
+        //probability to display
+        var probability_decimal = Number(Math.round(probability + 'e2') + 'e-2').toFixed(2);
+        //used for determining the width of the progressbar
+        var probability_int = Math.round(probability);
+
+        //the probabilities of the classifications are displayed using this piece of code.
+        $('#progressList').append("<h6>" + probability_decimal + "% " + "</h6><div class='progress'><div class='progress-bar progress-bar-custom text' " +
+            "role='progressbar' aria-valuenow='" + probability_int + "' aria-valuemin='0' aria-valuemax='100' " +
+            "style='width: " + probability_int + "%;'></div></div>")
+
+        $('#labelsList').append("<p>" + label + "&ensp;</p>")
+    }
 }
